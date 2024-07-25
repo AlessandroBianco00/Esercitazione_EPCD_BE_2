68733979657ad5c8eca1b74ff985b2fb2b9394e6
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-07-25 15:38:18 +0200

    Creata api customer, chiamata ajax per mostrare clienti

---
+++ HotelWebApp/Controllers/CustomerApiController.cs
@@ -0,0 +1,25 @@
+﻿using HotelWebApp.Dto;
+using HotelWebApp.Services.Dao;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        private readonly DbContext _dbContext;
+
+        public CustomerApiController(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ActionResult<IEnumerable<CustomerDto>> AllCustomers()
+        {
+            var customers = _dbContext.Customer.GetAllCustomers();
+            return Ok(customers);
+        }
+    }
+}
