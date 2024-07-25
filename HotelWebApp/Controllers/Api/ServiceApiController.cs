bc78181d42f2e787d54dbb99aba69d2cff3bccc7
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-07-25 21:08:24 +0200

    Select prenotazione funzionante, inizio api service

---
+++ HotelWebApp/Controllers/Api/ServiceApiController.cs
@@ -0,0 +1,33 @@
+﻿using HotelWebApp.Dto;
+using HotelWebApp.Services.Dao;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWebApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceApiController : ControllerBase
+    {
+        private readonly DbContext _dbContext;
+
+        public ServiceApiController(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ServiceDto>> AllServices()
+        {
+            var services = _dbContext.Service.GetAllServices();
+            return Ok(services);
+        }
+
+        [HttpGet("/id")]
+        public IActionResult ServiceById([FromBody] int id)
+        {
+            var service = _dbContext.Service.GetServiceById(id);
+            return Ok(service);
+        }
+    }
+}
