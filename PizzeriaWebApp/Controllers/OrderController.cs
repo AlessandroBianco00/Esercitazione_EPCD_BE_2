c9d29090078c394fd127812a3c1dc42cf620d11c
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-07-31 16:48:00 +0200

    Creato order controller e menu view

---
+++ PizzeriaWebApp/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzeriaWebApp.Context;
+using PizzeriaWebApp.Interfaces;
+
+namespace PizzeriaWebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IProductService _productService;
+        private readonly IIngredientService _ingredientService;
+        private readonly DataContext _ctx;
+
+        public OrderController(DataContext dbContext, ILogger<OrderController> logger, IProductService productService, IIngredientService ingredientService)
+        {
+            _logger = logger;
+            _ctx = dbContext;
+            _productService = productService;
+            _ingredientService = ingredientService;
+        }
+
+        public async Task<IActionResult> Menu()
+        {
+            var list = await _productService.GetAll();
+            return View(list);
+        }
+
+        public async Task<IActionResult> AddToOrder(int id)
+        {
+            var product = await _productService.GetAll();
+            ViewBag.Product = product;
+            return View();
+        }
+    }
+}
